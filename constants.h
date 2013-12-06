a1874ceb787b4715ed2b6573cb0024f8701b1b89
Team 1815 <macrobotics2014@gmail.com>
2013-12-05 20:17:43 -0500

    PID YO

--- constants.h
+++ constants.h
@@ -1,5 +1,6 @@
 // robot constants
-#define WHEEL_RADIUS 2 //inches
+#define WHEEL_RADIUS 2. //inches
 #define WHEEL_TO_ENCODER_RATIO 2.5 //absolute
+#define WHEEL_FUDGE_FACTOR 2.5
 #define ROBOT_WIDTH 14.5 //inches
 #define ROBOT_RADIUS ROBOT_WIDTH / 2 //inches
